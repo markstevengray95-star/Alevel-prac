e5d3d4695232d1d8ff381d7de6e0d8dc1136e77a
markstevengray95-star <markstevengray95@gmail.com>
2026-09-19 21:59:53 +0100

    Bump enhancement revision for resilient 3D viewer

--- lab-book-bootstrap.js
+++ lab-book-bootstrap.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const REV='20260919-live3d-111';
+const REV='20260919-live3d-112';
 const versioned=src=>`${src}?v=${REV}`;
 function loadStyle(href,key){if(document.querySelector(`link[data-${key}]`))return;const l=document.createElement('link');l.rel='stylesheet';l.href=href;l.dataset[key]='1';document.head.appendChild(l);}
 function addLabView(){
