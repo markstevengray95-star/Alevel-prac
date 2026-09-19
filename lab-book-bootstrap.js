f0ef098254287333d0744c1758f2750619edf2da
markstevengray95-star <markstevengray95@gmail.com>
2026-09-19 22:24:08 +0100

    Bump enhancement revision for photoreal 3D v13

--- lab-book-bootstrap.js
+++ lab-book-bootstrap.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const REV='20260919-physical3d-v12';
+const REV='20260919-photoreal3d-v13';
 const versioned=src=>`${src}?v=${REV}`;
 function loadStyle(href,key){if(document.querySelector(`link[data-${key}]`))return;const l=document.createElement('link');l.rel='stylesheet';l.href=href;l.dataset[key]='1';document.head.appendChild(l);}
 function addLabView(){
